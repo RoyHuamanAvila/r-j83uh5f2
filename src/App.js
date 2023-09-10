58155685c472f0ad0729e2ce726c183eab9a5d13
Roy Huaman <RHavila789@gmail.com>
2023-09-10 12:59:34 -0500

    feat: completed tests

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      count: 0
+    }
+  }
+
+  countCharacters = (e) => {
+    const nCharacters = e.target.value.length;
+    this.setState({count: nCharacters})
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.countCharacters}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
   }
